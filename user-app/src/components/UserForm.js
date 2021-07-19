6fad8ab5bf713972d36a419a9ade840fc8339698
prakashtimes <jyoti.das@timesinternet.in>
2021-07-19 12:19:20 +0530

    changed twoway data binding

--- user-app/src/components/UserForm.js
+++ user-app/src/components/UserForm.js
@@ -1,10 +1,14 @@
 import {useState} from "react"
 export default function UserForm(){
-    const[userform, setUserform]=useState({firstname:"Ram"});
+    const[userform, setUserform]=useState({firstname:"Ram"}); // hook function
+    const handleEvent =function(event){
+        console.log(event);
+        setUserform({...userform, firstname:event.target.value})
+    }
     return (
         <div>
             <h3>Create User</h3>
-            <input value={userform.firstname} />
+            <input value={userform.firstname} onChange={handleEvent} />
             
         </div>
     )
